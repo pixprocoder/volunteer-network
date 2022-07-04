43b644659f33048d0b9dcf2fc802e1dd185cfcfb
kabir <makkobir79@gmail.com>
2022-07-05 02:17:30 +0600

    some pages created and route setup completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import Navbar from "./pages/shared/Navbar";
+import Navbar from "./pages/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -8,10 +12,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </div>
   );

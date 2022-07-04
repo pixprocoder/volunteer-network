f59423fffa0f6e424410903ccf715d1c22afe38c
kabir <makkobir79@gmail.com>
2022-07-05 02:07:07 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./pages/shared/Navbar";
+
 function App() {
   return (
     <div>
-      <h1 className="text-blue-600">Welcome to volunteer-network</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/pages/shared/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="py-4 bg-yellow-50 shadow-lg">
+        <ul className="flex space-x-2">
+          <li>
+            <Link to="/home"> Home </Link>
+          </li>
+          <li>
+            <Link to="/about"> About </Link>
+          </li>
+          <li>
+            <Link to="/services"> Services </Link>
+          </li>
+          <li>
+            <Link to="/products"> Products </Link>
+          </li>
+          <li>
+            <Link to="/contact"> Contact </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
